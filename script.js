b63689af5660d22a81f5d63bdea81967d2a5969f
Ismar Sarac <ss2ismar@hotmail.com>
2022-04-07 16:09:07 -0700

    trying to get a best of 3 game going... so far not working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let playerSelection = 'rock';
+let playerSelection;
 let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
@@ -7,12 +7,40 @@ const rps = ['Rock','Paper','Scissors'];
 function computerPlay(){
     return rps[Math.floor(Math.random()*rps.length)];
 }
+function game(){
+    for (let i=0;i<5;i++){
+        playerSelection=prompt('make a choice!');
+        playRound(playerSelection,computerSelection);
+        console.log(i);
+        if (roundWinner == 'player'){
+            playerScore = playerScore++;
+            console.log(`you win!\n${playerSelection} beats ${computerSelection}.`);
+            return('win');
+        }
+        else if(roundWinner == 'bot'){
+            computerScore = computerScore++;
+            console.log(`you lose!\n${computerSelection} beats ${playerSelection}.`);
+            return('lose');
+        }
+        else if(roundWinner == 'tie'){
+            i-=1;
+            console.log('tie game');
+            return('tie');
+        }
+        else{
+            i-=1;
+            console.log('something went wrong');
+            return('neither');
+        }
+    }
+}
 
 function playRound(playerSelection,computerSelection){
     computerSelection=computerPlay().toLowerCase();
     playerSelection=playerSelection.toLowerCase();
 
     if (computerSelection == playerSelection){
+        roundWinner = 'tie';
         return('tie game');
     }
     else if (
@@ -20,6 +48,7 @@ function playRound(playerSelection,computerSelection){
         (computerSelection == 'paper' && playerSelection == 'rock') ||
         (computerSelection == 'scissors' && playerSelection == 'paper')
     ){
+        roundWinner ='bot';
         return(`you lose!\n${computerSelection} beats ${playerSelection}.`)
     }
     else if (
@@ -27,20 +56,13 @@ function playRound(playerSelection,computerSelection){
         (playerSelection == 'paper' && computerSelection == 'rock') ||
         (playerSelection == 'scissors' && computerSelection == 'paper')
     ){
+        roundWinner ='player';
         return(`you win!\n${playerSelection} beats ${computerSelection}.`)
     }
     else{
+        roundWinner='neither';
         return('something went wrong')
     }
 
 }
-
-// function game(){
-//     for (let i=0;i<5;i++){
-//         playRound(playerSelection,computerSelection);
-//         if (playRound() == win){
-            
-//         }   
-//     }
-// }
 console.log(playRound(playerSelection,computerSelection));
