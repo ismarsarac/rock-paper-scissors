0c8b7433d777320f98edc0615d74fc8fa5625cb0
Ismar Sarac <ss2ismar@hotmail.com>
2022-04-08 13:27:29 -0700

    finished best of 5 rps with console logs only

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let playerSelection;
+let playerSelection ='';
 let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
@@ -7,62 +7,55 @@ const rps = ['Rock','Paper','Scissors'];
 function computerPlay(){
     return rps[Math.floor(Math.random()*rps.length)];
 }
-function game(){
-    for (let i=0;i<5;i++){
-        playerSelection=prompt('make a choice!');
-        playRound(playerSelection,computerSelection);
-        console.log(i);
-        if (roundWinner == 'player'){
-            playerScore = playerScore++;
-            console.log(`you win!\n${playerSelection} beats ${computerSelection}.`);
-            return('win');
-        }
-        else if(roundWinner == 'bot'){
-            computerScore = computerScore++;
-            console.log(`you lose!\n${computerSelection} beats ${playerSelection}.`);
-            return('lose');
-        }
-        else if(roundWinner == 'tie'){
-            i-=1;
-            console.log('tie game');
-            return('tie');
-        }
-        else{
-            i-=1;
-            console.log('something went wrong');
-            return('neither');
-        }
-    }
-}
+
+
 
 function playRound(playerSelection,computerSelection){
+    playerSelection=prompt('make a choice!');
     computerSelection=computerPlay().toLowerCase();
     playerSelection=playerSelection.toLowerCase();
 
     if (computerSelection == playerSelection){
-        roundWinner = 'tie';
-        return('tie game');
+        console.log('tie game');
     }
     else if (
         (computerSelection == 'rock' && playerSelection == 'scissors') ||
         (computerSelection == 'paper' && playerSelection == 'rock') ||
         (computerSelection == 'scissors' && playerSelection == 'paper')
     ){
-        roundWinner ='bot';
-        return(`you lose!\n${computerSelection} beats ${playerSelection}.`)
+        computerScore++;
+        console.log(`you lose!\n${computerSelection} beats ${playerSelection}.`);
     }
     else if (
         (playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'paper' && computerSelection == 'rock') ||
         (playerSelection == 'scissors' && computerSelection == 'paper')
     ){
-        roundWinner ='player';
-        return(`you win!\n${playerSelection} beats ${computerSelection}.`)
+        playerScore++;
+        console.log(`you win!\n${playerSelection} beats ${computerSelection}.`);
     }
     else{
-        roundWinner='neither';
-        return('something went wrong')
+        console.log('something went wrong'); 
     }
 
 }
-console.log(playRound(playerSelection,computerSelection));
+
+do{
+    playRound(playerSelection,computerSelection);
+    console.log(playerScore);
+    console.log(computerScore);
+    if(playerScore===5||computerScore===5){
+        gameWinner();
+        break;
+    }
+}while((computerScore!=5)||(playerScore!=5));
+
+function gameWinner(){
+
+    if(playerScore>computerScore){
+        alert('You win!');
+    }
+    else{
+        alert('You lost!');
+    }
+}
